87248a86ad19d95a5ec7458458ce5a3f513c7456
Joel Reis <jreis@imaginarycloud.com>
2021-01-21 17:53:43 +0000

    Add export type

--- src/index.ts
+++ src/index.ts
@@ -105,7 +105,7 @@ export default class OtaClient {
     /**
      * List timestamp in distribution
      */
-    async getManifestTimestamp() {
+    async getManifestTimestamp(): Promise<number> {
         return (await this.manifest).timestamp;
     }
 
